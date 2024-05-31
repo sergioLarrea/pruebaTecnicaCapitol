b184c71425b810c5c11792c172a1781299e2737a
erredoble <erredoble2021@gmail.com>
2024-05-31 15:39:48 +0200

    La parte de llamadas a los endpoints hecha

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO spaceship (name, series) VALUES ('X-Wing', 'Star Wars');
+INSERT INTO spaceship (name, series) VALUES ('Millennium Falcon', 'Star Wars');
+INSERT INTO spaceship (name, series) VALUES ('USS Enterprise', 'Star Trek');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE spaceship (
+                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                           name VARCHAR(255),
+                           series VARCHAR(255)
+);
